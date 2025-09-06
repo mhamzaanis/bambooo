f8a6b267a322184d7e55f620aabe418a897f1141
mhamzaanis <mhamza29216@example.com>
2025-09-06 18:17:32 +0500

    Fix API to use ES modules instead of CommonJS require

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -265,4 +269,4 @@ app.get("/api/employees/:employeeId/dependents", (req, res) => {
 });
 
 // Export as Vercel serverless function
-module.exports = app;
+export default app;
